aff096bb5140a699c7fe81fd702d76e9ef67eeb5
Gowtham M <gowtham.1392@gmail.com>
2019-11-16 19:14:30 +0530

    Complete todo app

--- typescript-app/src/App.tsx
+++ typescript-app/src/App.tsx
@@ -1,15 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, Fragment } from "react";
 
 import "./App.css";
 
 type FormElement = React.FormEvent<HTMLFormElement>;
+interface TodoProps {
+  text: string;
+  complete: boolean;
+}
 
 const App = () => {
-  const [value, setValue] = useState("");
+  const [value, setValue] = useState<string>("");
+  const [todos, setTodos] = useState<TodoProps[]>([]);
 
   const handleSubmit = (e: FormElement) => {
     e.preventDefault();
+    addTodo(value);
     setValue("");
+  };
+
+  const addTodo = (text: string): void => {
+    const todoArray: TodoProps[] = [...todos, { text, complete: false }];
+    setTodos(todoArray);
+  };
+
+  const completeTodo = (index: number): void => {
+    const newTodo: TodoProps[] = [...todos];
+    newTodo[index].complete = !newTodo[index].complete;
+    setTodos(newTodo);
+  };
+
+  const deleteTodo = (index: number): void => {
+    const newTodos: TodoProps[] = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
   };
 
   return (
@@ -24,6 +47,34 @@ const App = () => {
         />
         <button type="submit">Add ToDo</button>
       </form>
+      <section>
+        {todos.map((todo: TodoProps, index: number) => {
+          return (
+            <Fragment key={index}>
+              <div>
+                <div
+                  style={{
+                    textDecoration: todo.complete ? "line-through" : undefined
+                  }}
+                >
+                  {todo.text}
+                </div>
+                <button type="button" onClick={() => completeTodo(index)}>
+                  {todo.complete ? "Complete" : "Incomplete"}
+                </button>
+                <button
+                  type="button"
+                  onClick={() => {
+                    deleteTodo(index);
+                  }}
+                >
+                  &times;
+                </button>
+              </div>
+            </Fragment>
+          );
+        })}
+      </section>
     </div>
   );
 };
