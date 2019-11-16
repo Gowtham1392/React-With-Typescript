e7db340dea96a5f6c7c399ccd52aeff0dae060b6
Gowtham M <gowtham.1392@gmail.com>
2019-11-16 17:50:06 +0530

    After Hook

--- typescript-app/src/App.tsx
+++ typescript-app/src/App.tsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./App.css";
 
-function App() {
-  return <div>HEllo</div>;
-}
+type FormElement = React.FormEvent<HTMLFormElement>;
+
+const App = () => {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = (e: FormElement) => {
+    e.preventDefault();
+    setValue("");
+  };
+
+  return (
+    <div>
+      <h1>ToDo List</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={value}
+          onChange={e => setValue(e.target.value)}
+          required
+        />
+        <button type="submit">Add ToDo</button>
+      </form>
+    </div>
+  );
+};
 
 export default App;
